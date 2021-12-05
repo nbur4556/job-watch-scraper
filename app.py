c6015c85f87feac4216143568fe2e38cf90ef22b
nbur4556 <nbur4556@gmail.com>
2021-12-04 20:03:09 -0600

    feat: write results to csv

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from page_crawler import get_url, load_page
+import csv
 
 
 def main():
-    url = get_url('Javascript', 'Austin')
+    keyword = 'Javascript'
+    location = 'Austin'
+
+    url = get_url(keyword, location)
     bs_page = load_page(url)
     job_count = parse_result(bs_page)
 
-    print(job_count)
+    write_result(keyword, location, job_count)
 
 
 # Return job count as an integer
@@ -15,6 +19,13 @@ def parse_result(page):
     result_str = str(result_loc[0])
     count = result_str[58:-11]
     return int(count)
+
+
+def write_result(keyword, location, count):
+    writer = csv.writer(open('results.csv', mode='a', newline=''),
+                        delimiter=',', quotechar='"')
+
+    writer.writerow([keyword, location, count])
 
 
 # Run Application

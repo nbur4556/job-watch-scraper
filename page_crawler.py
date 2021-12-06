e9e6e1f2665f71f9027af0dc7a521a44a00aff64
nbur4556 <nbur4556@gmail.com>
2021-12-06 16:25:02 -0600

    feat: add getters and setters to page crawler parameters

--- page_crawler.py
+++ page_crawler.py
@@ -1,3 +1,4 @@
+from os import EX_PROTOCOL
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -20,3 +21,23 @@ class Page_Crawler:
         match = re.search(self.exp, str(self.page))
         count = int(match.group()[self.trim_start:-self.trim_end])
         return count
+
+    # GETTERS / SETTERS
+    def get_url(self):
+        return self.url
+
+    def set_url(self, search_term, location):
+        self.url = f'https://www.indeed.com/jobs?q={search_term}&l={location}%2C%20TX&fromage=1'
+
+    def get_expression(self):
+        return self.exp
+
+    def set_expression(self, exp):
+        self.exp = exp
+
+    def get_trim(self):
+        return (self.trim_start, self.trim_end)
+
+    def set_trim(self, trim_start, trim_end):
+        self.trim_start = trim_start
+        self.trim_end = trim_end
